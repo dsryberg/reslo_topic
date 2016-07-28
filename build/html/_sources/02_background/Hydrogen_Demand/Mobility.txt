e4597f0bd83eae4f44ece265be28ec4f0d961aba
Severin Ryberg <SevBerg129@Gmail.com>
2016-07-28 13:46:18 +0200

    Theme change
    Theme has been changed to sphinx_rtd_theme which looks much nicer and is mobile friendly (because why not?). Also added a little more structure

---
+++ build/html/_sources/02_background/Hydrogen_Demand/Mobility.txt
@@ -0,0 +1,16 @@
+Mobility Demand
+===============
+
+Some info
+
+
+Fuel Cell Elevtric Vehicle - Light Duty Vehicles
+------------------------------------------------
+
+more info
+
+
+Fuel Cell Elevtric Vehicle - Light Goods Vehicles
+-------------------------------------------------
+
+even MORE info!
--- source/conf.py
+++ source/conf.py
@@ -16,6 +16,9 @@
 import sys
 import os
 
+# import sphinx_rtd_theme for theme support
+import sphinx_rtd_theme
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -104,19 +107,23 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
+html_theme_options = {
+    'collapse_navigation': False,
+    'display_version': False
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "Residual Load Topic"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
